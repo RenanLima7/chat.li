8060296e19931f8242097ae473bb96eff792b4fb
Renan Lima <86834473+RenanLima7@users.noreply.github.com>
2022-07-13 16:02:15 -0300

    Update MessageRepository.java

--- src/main/java/com/ufc/web/chatly/repository/MessageRepository.java
+++ src/main/java/com/ufc/web/chatly/repository/MessageRepository.java
@@ -10,4 +10,8 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 	Iterable<Message> findBySenderId(Long sender);
 	
 	Iterable<Message> findByAddresseeId(Long addressee);
+	
+	@Query(nativeQuery = true, value = "SELECT * FROM message "
+	+ "WHERE userId = :userId AND contactId = :contactId")
+	Iterable<Message> findBySenderIdAndAddresseeId(senderId, addresseeId);
 }
